c865b011797cef54c602e627f9471678a351e3e2
Dmytro Mamenko <mamenko.dv@gmail.com>
2021-10-14 18:38:23 +0200

    task#0: add  player creat function

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log("HELLO ZAR");
+class Player {
+	constructor(name, hp, img, weapon) {
+		this.name = name;
+		this.hp = hp;
+		this.img = img;
+		this.weapon = ["default nozhik", "default  lopata", "default lomik"];
+	}
+
+	attack() {
+		console.log(`${this.name} fight`);
+	}
+}
+const player1 = new Player(
+	"Scorpion",
+	120,
+	"http://reactmarathon-api.herokuapp.com/assets/scorpion.gif"
+);
+const player2 = new Player(
+	"Sonya",
+	110,
+	"http://reactmarathon-api.herokuapp.com/assets/sonya.gif"
+);
+player1.attack();
+player2.attack();
